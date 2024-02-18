3f07e661f54746ef3b1010289bdec306a55e0407
arturiamu <arturiamumu@gmail.com>
2024-02-18 11:33:45 +0800

    factory,factory method

---
+++ 02-factory/factory.go
@@ -0,0 +1,31 @@
+package factory
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+// jsonRuleConfigParser jsonRuleConfigParser
+type jsonRuleConfigParser struct{}
+
+// Parse Parse
+func (J jsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+// yamlRuleConfigParser yamlRuleConfigParser
+type yamlRuleConfigParser struct{}
+
+// Parse Parse
+func (Y yamlRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+func NewIRuleConfigParser(t string) IRuleConfigParser {
+	switch t {
+	case "json":
+		return jsonRuleConfigParser{}
+	case "yaml":
+		return yamlRuleConfigParser{}
+	}
+	return nil
+}
---
+++ 02-factory/factory_method.go
@@ -0,0 +1,31 @@
+package factory
+
+// IRuleConfigParserFactory 工厂方法接口
+type IRuleConfigParserFactory interface {
+	CreateParser() IRuleConfigParser
+}
+
+// yamlRuleConfigParserFactory yamlRuleConfigParser 的工厂类
+type yamlRuleConfigParserFactory struct{}
+
+func (y yamlRuleConfigParserFactory) CreateParser() IRuleConfigParser {
+	return yamlRuleConfigParser{}
+}
+
+// jsonRuleConfigParserFactory jsonRuleConfigParser 的工厂类
+type jsonRuleConfigParserFactory struct{}
+
+func (j jsonRuleConfigParserFactory) CreateParser() IRuleConfigParser {
+	return jsonRuleConfigParser{}
+}
+
+// NewIRuleConfigParserFactory 用一个简单工厂封装工厂方法
+func NewIRuleConfigParserFactory(t string) IRuleConfigParserFactory {
+	switch t {
+	case "json":
+		return jsonRuleConfigParserFactory{}
+	case "yaml":
+		return yamlRuleConfigParserFactory{}
+	}
+	return nil
+}
